c3feb99be752b3a9aec3cf3781194caf1146a598
jkbwtk <jakub.wiatek@student.pk.edu.pl>
2022-11-14 23:47:37 +0100

    Added basic project structure

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+
+require 'Routing.php';
+
+$path = trim($_SERVER['REQUEST_URI'], '/');
+$path = parse_url($path, PHP_URL_PATH);
+
+Router::get('', 'DefaultController');
+
+Router::run($path);
