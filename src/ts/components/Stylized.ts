a1be342e4e0d3382b8c9ad8598eb39269a41e192
jkbwtk <jakub.wiatek@student.pk.edu.pl>
2022-12-03 18:28:09 +0100

    Added support for multiple styles and events

--- src/ts/components/Stylized.ts
+++ src/ts/components/Stylized.ts
@@ -1,23 +1,54 @@
-export abstract class Stylized {
-  protected abstract style: HTMLLinkElement;
+import { EventEmitter } from 'events';
 
-  protected static createStyle(url: string): HTMLLinkElement {
+
+export abstract class Stylized extends EventEmitter {
+  protected abstract styles: HTMLLinkElement[] | HTMLLinkElement;
+
+  protected static createStyle(url: string, integrity?: string, crossorigin?: string): HTMLLinkElement {
     const element = document.createElement('link');
     element.rel = 'stylesheet';
     element.href = url;
 
+    if (typeof integrity === 'string') {
+      element.integrity = integrity;
+    }
+
+    if (typeof crossorigin === 'string') {
+      element.crossOrigin = crossorigin;
+    }
+
     return element;
   }
 
-  protected injectStyle(): void {
-    if (!document.head.contains(this.style)) {
-      document.head.appendChild(this.style);
+  protected injectStyles(): void {
+    if (Array.isArray(this.styles)) {
+      for (const style of this.styles) {
+        this.injectStyle(style);
+      }
+    } else {
+      this.injectStyle(this.styles);
     }
   }
 
-  protected removeStyle(): void {
-    if (document.head.contains(this.style)) {
-      document.head.removeChild(this.style);
+  private injectStyle(style: HTMLLinkElement) {
+    if (!document.head.contains(style)) {
+      document.head.appendChild(style);
+    }
+  }
+
+  protected removeStyles(): void {
+    if (Array.isArray(this.styles)) {
+      for (const style of this.styles) {
+        this.removeStyle(style);
+      }
+    } else {
+      this.removeStyle(this.styles);
+    }
+  }
+
+  private removeStyle(style: HTMLLinkElement) {
+    if (document.head.contains(style)) {
+      document.head.removeChild(style);
     }
   }
 }
