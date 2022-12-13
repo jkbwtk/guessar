634713b9624bf319c62f325f0fb70ec38f078371
jkbwtk <jakub.wiatek@student.pk.edu.pl>
2022-12-13 02:32:35 +0100

    Added experimental way of creating HTML elements

---
+++ src/ts/components/Node.ts
@@ -0,0 +1,62 @@
+export class Node<T extends keyof HTMLElementTagNameMap> {
+  private element: HTMLElementTagNameMap[T];
+  private failed = false;
+
+  constructor(tagName: T) {
+    this.element = document.createElement(tagName);
+  }
+
+  public map(func: (v: HTMLElementTagNameMap[T]) => void): this {
+    if (this.failed) return this;
+
+    try {
+      func(this.element);
+    } catch (error) {
+      this.failed = true;
+    }
+
+    return this;
+  }
+
+  public addClass(...tokens: string[]): this {
+    return this.map(((v) => v.classList.add(...tokens)));
+  }
+
+  public removeClass(...tokens: string[]): this {
+    return this.map(((v) => v.classList.remove(...tokens)));
+  }
+
+  public appendChild(node: globalThis.Node): this {
+    return this.map((v) => v.appendChild(node));
+  }
+
+  public innerText(text: string): this {
+    return this.map((v) => v.innerText = text);
+  }
+
+  public innerHTML(html: string): this {
+    return this.map((v) => v.innerHTML = html);
+  }
+
+  public setStyle<S extends keyof CSSStyleDeclaration>(style: S, value: CSSStyleDeclaration[S]): this {
+    return this.map((v) => v.style[style] = value);
+  }
+
+  public unwrapUnsafe(): HTMLElementTagNameMap[T] {
+    return this.element;
+  }
+
+  public unwrapSafe(): HTMLElementTagNameMap[T] {
+    if (this.failed) throw new Error('Node failed to unwrap');
+
+    return this.element;
+  }
+
+  public didFail(): boolean {
+    return this.failed;
+  }
+
+  public static src = <T extends HTMLElementTagNameMap['img']>(src: string): ((v: T) => void) => {
+    return ((v) => v.src = src);
+  };
+}
